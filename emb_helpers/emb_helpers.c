afebff3f3809c0e400fc8eb5d084a3a08d0c725d
oxnh <77563973+Hardware7253@users.noreply.github.com>
2026-02-25 10:58:53 +1300

    Added clamp to map function output

--- emb_helpers/emb_helpers.c
+++ emb_helpers/emb_helpers.c
@@ -5,5 +5,13 @@
 // The full int32 range can't be used because a multiply by 1000 is performed to keep precision
 // The output range should be less than 2 million to avoid integer overflow errors
 inline int32_t map(int32_t num, int32_t num_low, int32_t num_high, int32_t out_low, int32_t out_high) {
-    return ((1000 * (num - num_low) / (num_high - num_low)) * (out_high - out_low) / 1000 + out_low);
+    int32_t out = ((1000 * (num - num_low) / (num_high - num_low)) * (out_high - out_low) / 1000 + out_low);
+
+    // Clamp output
+    if (out > out_high) {
+        return out_high;
+    } else if (out < out_low) {
+        return out_low;
+    }
+    return out;
 }
